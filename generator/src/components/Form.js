20593062317631216208dec53a3cc2856c2ec30a
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-09-28 12:43:41 +0200

    displaying data from form using state

--- generator/src/components/Form.js
+++ generator/src/components/Form.js
@@ -6,10 +6,12 @@ const [formData, setFormData] = React.useState(
     {firstName:'',lastName: '',reminder: false}
 )
 const handleChange =(event) =>{
+    const {name,value,type, checked}=event.target
     setFormData(prevFormData => {
         return {
             ...prevFormData,
-            [event.target.name]: event.target.value
+            // [event.target.name]: event.target.value
+            [name]: type === "checkbox" ? checked : value
         }
     })
 
@@ -25,7 +27,7 @@ const onsubmit = (event) => {
     setFormData({
       firstName: '',
       lastName: '',
-      reminder: false
+      reminder: true
     })
   }
  
@@ -33,15 +35,15 @@ const onsubmit = (event) => {
     <form className='mt-16' onSubmit={onsubmit}>
         <div className='mb-5'>
             <label className='mr-5'>FirstName</label>
-            <input type='text' placeholder='First Name' className='border border-gray-600' name='firstName'  onChange={handleChange} ></input>
+            <input type='text' placeholder='First Name' className='border border-gray-600' name='firstName'  onChange={handleChange} value={formData.firstName} ></input>
         </div>
         <div className='mb-5'>
             <label className='mr-5'>LastName</label>
-            <input type='text' placeholder='Last Name' className='border border-gray-600' name='lastName'   onChange={handleChange}></input>
+            <input type='text' placeholder='Last Name' className='border border-gray-600' name='lastName'   onChange={handleChange} value={formData.lastName}></input>
         </div>
         <div className='mb-5'>
             <label className='mr-5'>Set Reminder</label>
-            <input type='checkbox'   className='border border-gray-600' name='reminder'  onChange={handleChange}></input>
+            <input type='checkbox'   className='border border-gray-600' name='reminder'  onChange={handleChange} value={formData.reminder}></input>
         </div>
         <button type='submit' className='px-6 py-3 bg-teal-400'> Submit</button>
     </form>
--- generator/src/components/Meme.js
+++ generator/src/components/Meme.js
@@ -4,14 +4,21 @@ import datas from '../utils/memeData.json'
 
 const Meme = () => {
 
-  const [meme, setMeme] = React.useState({
-    topText: "",
-    bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg" 
-})
-
+const [meme, setMeme] = React.useState(
+    { top: "",bottom: "",randomImage: "http://i.imgflip.com/1bij.jpg" }
+)
 const [allMemeImages,setAllMemeImages] = React.useState(datas)
 
+const changeMeme = (event)=>{
+  const {name,type,value} =event.target
+  setMeme(prevFormMeme =>{
+    return{
+      ...prevFormMeme,
+      [name]: type === "text" ? value : undefined
+    }
+    
+  })
+}
 
     const onClick =(event)=>{
       event.preventDefault()
@@ -28,12 +35,13 @@ const [allMemeImages,setAllMemeImages] = React.useState(datas)
   return (
     <div>
       <main className='flex flex-col justify-center items-center '>
-  <form className='grid grid-cols-2  mt-12 w-1/2' >
-    <input className="shadow appearance-none border rounded text-gray-700 mr-3 leading-tight focus:outline-none focus:shadow-outline" id='top' type="text" placeholder="" autoComplete="off" />
-    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id='bottom'  type="text" placeholder="Bottom text" autoComplete="off" />
+     <form className='grid grid-cols-2  mt-12 w-1/2' >
+    <input className="shadow appearance-none border rounded text-gray-700 mr-3 leading-tight focus:outline-none focus:shadow-outline" id='top' type="text" placeholder="" autoComplete="off" name='top'  onChange={changeMeme} value ={meme.top} />
+    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id='bottom'  type="text" placeholder="Bottom text" autoComplete="off" name='bottom'  onChange={changeMeme} value={meme.bottom} />
     <button onClick={onClick} className='bg-[#8324a5] py-2 px-3 grid col-span-2 mt-12' >Get a new meme image</button>
   </form>
-  <img src={meme.randomImage} className='w-1/2'/>
+  <img src={meme.randomImage} className='w-1/2 relative'/>
+  <p className='absolute top-80 text-white'> {meme.top} {meme.bottom}</p>
 </main>
 
     </div>
